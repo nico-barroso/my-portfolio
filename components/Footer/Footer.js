4be4a18c39838a494650a5e57d3eeaa0c66cae0e
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2024-07-16 20:45:37 +0100

    z-index updated in button linkedin

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,18 +2,16 @@ import "./Footer.css";
 import { Button } from "../Button/Button";
 
 export const Footer = () => `
-
-
-<div>
-${Button(
-  "/assets/github-mark.svg",
-  "GitHub",
-  "https://github.com/nico-barroso"
-)}
-${Button(
-  "./assets/LI-In-Bug.png",
-  "LinkedIn",
-  "https://linkedin.com/in/nico-barroso"
-)}
+<div class="footer">
+  ${Button(
+    "/assets/github-mark.svg",
+    "GitHub",
+    "https://github.com/nico-barroso"
+  )}
+  ${Button(
+    "./assets/LI-In-Bug.png",
+    "LinkedIn",
+    "https://linkedin.com/in/nico-barroso"
+  )}
 </div>
 `;
--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -7,14 +7,15 @@ export const Home = () => {
   cleanPage(main);
   main.innerHTML = `
     <section class="home">
-    <p>Hola, soy</p>
-    <h1>Nico Barroso</h1>
-<p>Soy un desarrollador de software en España, especializado en el diseño y desarrollo de productos. Me apasiona crear productos interactivos que mejoren la vida de las personas o cuenten historias. He trabajado en diversos proyectos de comunicación y consumo desde la universidad, adquiriendo un amplio conocimiento en mi campo. Actualmente, estoy perfeccionando mis habilidades con un técnico en desarrollo de aplicaciones y una especialización en Full-Stack.</p>   
-    <a href="mailto:josenicolasbarrosogarcia@gmail.com"><img class="mail-icon" src="/assets/mail-svgrepo-com.svg" alt="Icono de correo">¡No te olvides de decir hola! →</a>
-<video autoplay loop muted playsinline webkit-playsinline>
-    <source src="/assets/PruebaPatoHVEC-1.mov" type="video/mp4"/> 
-</video>
+      <p>Hola, soy</p>
+      <h1>Nico Barroso</h1>
+      <p>Soy un desarrollador de software en España, especializado en el diseño y desarrollo de productos. Me apasiona crear productos interactivos que mejoren la vida de las personas o cuenten historias. He trabajado en diversos proyectos de comunicación y consumo desde la universidad, adquiriendo un amplio conocimiento en mi campo. Actualmente, estoy perfeccionando mis habilidades con un técnico en desarrollo de aplicaciones y una especialización en Full-Stack.</p>   
+      <a href="mailto:josenicolasbarrosogarcia@gmail.com"><img class="mail-icon" src="/assets/mail-svgrepo-com.svg" alt="Icono de correo">¡No te olvides de decir hola! →</a>
+      <video autoplay loop muted playsinline webkit-playsinline>
+        <source src="/assets/PruebaPatoHVEC-1.mov" type="video/mp4"/> 
+      </video>
     </section>`;
+
   const footer = document.querySelector("footer");
   footer.innerHTML = Footer();
 };
