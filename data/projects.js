16eabcbd4ebb79819fd84d9ee69cbf747225d0be
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2024-07-24 18:47:29 +0100

    Cambios en legibilidad y tamaño de archivo

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "MNTN - A hiking experience",
-    description: "Esta es la descripción de mi primer proyecto",
+    description: "Desarrollo del front-end de una web de ",
     videoImg: "/assets/project-video/project01.mp4",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/nico-barroso/entrega-flexbox",
@@ -9,10 +9,20 @@ export const projects = [
   },
   {
     title: "Stretchbook",
-    description: "Esta es la descripción de mi primer proyecto",
+    description:
+      "Ideación, diseño y desarrollo de la interfaz visual de una aplicación móvil de asistencia al bienestar.",
     videoImg: "/assets/project-video/project02.mp4",
     tech: ["Diseño UX", "Diseño UI", "Jetpack Compose", "Kotlin"],
     github: "https://github.com/nico-barroso/entrega-flexbox",
     link: "https://flexbox-nico.vercel.app",
+  },
+  {
+    title: "Zenniopticals",
+    description:
+      "Desarrollo de un ecommerce de venta de gafas usando JavaScript.",
+    videoImg: "/assets/project-video/zenniopticals.mp4",
+    tech: ["HTML5", "CSS", "Js-DOM"],
+    github: "https://github.com/nico-barroso/galeria-dinamica-dom",
+    link: "https://zenni-dom-practice.vercel.app/",
   },
 ];
